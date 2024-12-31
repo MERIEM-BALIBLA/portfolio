6b279f345440b9b04c6bd13a3bb904012a615d6f
balibla meriem <balibla.meriem@gmail.com>
2024-12-31 11:55:24 +0100

    refactoring css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     });
   };
   return (
-    <div className="bg-zinc-200 relative ">
+    <div className="bg-zinc-200 relative md:px-32">
      <Header/>
       <Profile/>
       <Skils/>
